a4bae9fd62a59d5f481e9c43da9dc68e72470e60
Leon1435 <leonidasdamrong0828@gmail.com>
2026-03-13 23:51:48 +0800

    fix style.css link issue

--- public/js/components.js
+++ public/js/components.js
@@ -25,7 +25,7 @@
 
   const headConfig = {
     bootstrapHref: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-    customCssHref: 'styles.css',
+    customCssHref: 'style/styles.css',
   };
 
   /* ──────────────────────────────────────────
